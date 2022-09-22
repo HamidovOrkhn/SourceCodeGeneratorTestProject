a221eab385be12d896d145e4a4d4c1338330a36e
HamidovOrkhn <orxan.hamidov.orxan.hamidov@mail.ru>
2022-09-22 15:48:51 +0400

    initial

---
+++ SourceCodeGenerator/SourceCodeGenerator/Generator/DatabaseGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using SourceCodeGenerator.Models;
+
+namespace SourceCodeGenerator.Generator
+{
+    [Generator]
+    public class DatabaseGenerator : ISourceGenerator
+    {
+    
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var sourceBuilder = new StringBuilder(@"
+using System;
+namespace SourceCodeGenerator
+{
+    public static class HelloWorld
+    {
+        public static void SayHello() 
+        {
+            Console.WriteLine(""Hello from generated code!"");
+            Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
+");
+
+            // using the context, get a list of syntax trees in the users compilation
+            var syntaxTrees = context.Compilation.SyntaxTrees;
+
+            // add the filepath of each tree to the class we're building
+            foreach (SyntaxTree tree in syntaxTrees)
+            {
+                sourceBuilder.AppendLine($@"Console.WriteLine(@"" - {tree.FilePath}"");");
+            }
+
+            // finish creating the source to inject
+            sourceBuilder.Append(@"
+        }
+    }
+}");
+
+            // inject the created source into the users compilation
+            context.AddSource("helloWorldGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        
+    }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            //if (!Debugger.IsAttached)
+            //{
+            //    Debugger.Launch();
+            //}
+
+        }
+    }
+}
